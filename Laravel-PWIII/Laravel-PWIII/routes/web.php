faac08a463c487962c506d0c7dac44e87ce9281f
Júlio César <jc8794951@gmail.com>
2026-08-20 20:49:33 -0300

    Delete Laravel-PWIII/Laravel-PWIII directory

--- Laravel-PWIII/Laravel-PWIII/routes/web.php
+++
@@ -1,7 +0,0 @@
-<?php
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\UserController;
-Route::get('/', fn()=>view('welcome'));
-Route::get('/usuarios',[UserController::class,'index'])->name('users.index');
-Route::get('/cadastro',[UserController::class,'create'])->name('users.create');
-Route::post('/cadastro',[UserController::class,'store'])->name('users.store');
